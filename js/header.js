825f1a2b425a3d00eb152c32056ec922293fa60f
Bruno Wotzke <brunoricardowotzke@gmail.com>
2023-02-15 12:40:45 -0300

    .

---
+++ js/header.js
@@ -0,0 +1,18 @@
+const body = document.body;
+const head = document.head;
+
+
+head.innerHTML += '<link rel="stylesheet" href="css/header.css">'
+
+let get = new XMLHttpRequest();
+get.open("GET", "../components/header.html");
+get.send();
+
+get.onload = () => {
+    if(get.status == 200){
+        let newH = get.responseText;
+        console.log(newH)
+
+        body.innerHTML += newH;
+    }
+}
