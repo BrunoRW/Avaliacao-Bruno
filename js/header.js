5fe31ac557a49b80eebe2d07d578a50256d380fe
Bruno Wotzke <brunoricardowotzke@gmail.com>
2023-02-16 21:58:29 -0300

    .

--- js/header.js
+++ js/header.js
@@ -1,19 +1,18 @@
 const body = document.body;
 const head = document.head;
 
-
-head.innerHTML += '<link rel="stylesheet" href="css/header.css">'
+head.innerHTML += '<link rel="stylesheet" href="css/header.css">';
 
 let get = new XMLHttpRequest();
 get.open("GET", "../components/header.html");
 get.send();
 
 get.onload = () => {
-    if(get.status == 200){
-        let newH = get.responseText;
-        console.log("🚀 ~ file: header.js:14 ~ responseText", responseText)
-        console.log(newH)
+  if (get.status == 200) {
+    let newH = get.responseText;
+    console.log("🚀 ~ file: header.js:14 ~ newH", newH);
+    console.log(newH);
 
-        body.innerHTML += newH;
-    }
-}
+    body.innerHTML += newH;
+  }
+};
